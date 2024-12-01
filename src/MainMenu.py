14527234bceff9cd72db5757c2a763cf6c411547
AnastsiaMir <anastasia.mironova7@gmail.com>
2024-12-02 00:33:30 +0300

    Test module added

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -7,12 +7,18 @@ class MainMenu(QMenuBar):
 
         teacher_menu = self.addMenu('Учитель')
 
+        test_menu = self.addMenu('Задача')
+
 
         self.__about = help_menu.addAction('О программе ...')
         self.__about_qt = help_menu.addAction('О библиотеке Qt...')
         self.__teacher_add = teacher_menu.addAction('Добавить')
         self.__teacher_update = teacher_menu.addAction('Редактировать')
         self.__teacher_delete = teacher_menu.addAction('Удалить')
+
+        self.__test_add = test_menu.addAction('Добавить')
+        self.__test_delete = test_menu.addAction('Удалить')
+
 
     @property
     def about(self):
@@ -33,3 +39,11 @@ class MainMenu(QMenuBar):
     @property
     def teacher_delete(self):
         return self.__teacher_delete
+
+    @property
+    def test_add(self):
+        return self.__test_add
+
+    @property
+    def test_delete(self):
+        return self.__test_delete
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -3,6 +3,8 @@ from PyQt6.QtCore import pyqtSlot
 from PyQt6.QtWidgets import QMainWindow, QMessageBox, QWidget, QVBoxLayout, QPushButton
 from MainMenu import MainMenu
 from Teacher import View
+import Teacher
+import Test
 
 
 class MainWindow(QMainWindow):
@@ -13,28 +15,31 @@ class MainWindow(QMainWindow):
         self.setMenuBar(main_menu)
 
         w = QWidget(parent=self)
-        view = View(parent=w)
+        view = Test.View(parent=w)
         main_menu.about.triggered.connect(self.about)
         main_menu.about_qt.triggered.connect(self.about_qt)
 
-        main_menu.teacher_add.triggered.connect(view.add)
-        main_menu.teacher_update.triggered.connect(view.update)
-        main_menu.teacher_delete.triggered.connect(view.delete)
+        # main_menu.teacher_add.triggered.connect(view.add)
+        # main_menu.teacher_update.triggered.connect(view.update)
+        # main_menu.teacher_delete.triggered.connect(view.delete)
+
+        main_menu.test_add.triggered.connect(view.add)
+        main_menu.test_delete.triggered.connect(view.delete)
 
 
         layout = QVBoxLayout()
         layout.addWidget(view)
-        add_button = QPushButton('Добавить', parent=w)
-        add_button.clicked.connect(view.add)
-        update_button = QPushButton('Изменить', parent=self)
-        update_button.clicked.connect(view.update)
-        delete_button = QPushButton('Удалить', parent=self)
-        delete_button.clicked.connect(view.delete)
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(add_button)
-        button_layout.addWidget(update_button)
-        button_layout.addWidget(delete_button)
-        layout.addLayout(button_layout)
+        # add_button = QPushButton('Добавить', parent=w)
+        # add_button.clicked.connect(view.add)
+        # update_button = QPushButton('Изменить', parent=self)
+        # update_button.clicked.connect(view.update)
+        # delete_button = QPushButton('Удалить', parent=self)
+        # delete_button.clicked.connect(view.delete)
+        # button_layout = QHBoxLayout()
+        # button_layout.addWidget(add_button)
+        # button_layout.addWidget(update_button)
+        # button_layout.addWidget(delete_button)
+        # layout.addLayout(button_layout)
         w.setLayout(layout)
         self.setCentralWidget(w)
 
